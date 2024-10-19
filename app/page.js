2eeb08b5e75652a2d72a0eb36a8c88e53ff4840f
Today-Asked <mao930710@gmail.com>
2024-10-19 22:58:37 +0800

    solve conflict

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect } from "react";
 import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
@@ -9,10 +8,14 @@ import TimeSlots from '@/components/calendar/TimeSlots';
 import LunarInfo from "@/components/calendar/LunarInfo";
 import { Button } from "@/components/ui/button";
 // import GoogleCalendarButton from '@/components/calendar/GoogleCalendarButton';
-
-
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useAuth } from '@/contexts/AuthContext';
 
 export default function Home() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const { login, user } = useAuth();
+
     useEffect(() => {
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', () => {
@@ -25,32 +28,46 @@ export default function Home() {
                     });
             });
         }
-    }, []);
+        const token = searchParams.get('token');
+        if (token) {
+        login(token);
+        // Remove the token from the URL
+        router.replace('/');
+        }
+        else {
+            alert('登入以使用所有功能！');
+            window.location.href = '/login';
+        }
+    }, [searchParams, login, router]);  
+    
     
     const test = () => {
         window.location.href = "https://ef91-140-113-136-220.ngrok-free.app/login";
     }
 
 
-    return (
-        <div className="h-screen">
-            <Card className="max-w-md mx-auto h-full bg-white relative rounded-2xl">
-                <CardContent className="p-0 h-full rounded-2xl">
-                    <Button onClick={test}>
-                        hiiiiii
-                    </Button>
-                    <DateHeader />
-                    <div className="w-full bg-[#13492f] h-[0.7rem] mb-2">
-                    </div>
-                    <div className="border border-[0.75rem] border-[#13492f] h-[35rem]">
-                        <LunarInfo />
-                        <TimeSlots />
-                    </div>
-                    <div className="w-full bg-[#13492f] h-[0.7rem] mt-2">
-                    </div>
-                    {/* <GoogleCalendarButton /> */}
-                </CardContent>
-            </Card>
-        </div>
-    );
+    if(user){
+        return (
+            <div className="h-screen">
+                <Card className="max-w-md mx-auto h-full bg-white relative rounded-2xl">
+                    <CardContent className="p-0 h-full rounded-2xl">
+                        <Button onClick={test}>
+                            hiiiiii
+                        </Button>
+                        <DateHeader />
+                        <div className="w-full bg-[#13492f] h-[0.7rem] mb-2">
+                        </div>
+                        <div className="border border-[0.75rem] border-[#13492f] h-[35rem]">
+                            <LunarInfo />
+                            <TimeSlots />
+                        </div>
+                        <div className="w-full bg-[#13492f] h-[0.7rem] mt-2">
+                        </div>
+                        {/* <GoogleCalendarButton /> */}
+                    </CardContent>
+                </Card>
+            </div>
+        );
+    }
+    return null;
 }
